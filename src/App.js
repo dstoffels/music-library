fbbf87bba5ba7e0e0557457f6f7eaa5a172d0132
dstoffels <dan.stoffels@gmail.com>
2022-02-14 14:19:49 -0600

    updated App.js to handle search criterion, updated SongTable to filter based on search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
 
+import SongTable from './Components/SongTable/SongTable.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import SearchBar from './Components/SearchBar/SearchBar.jsx';
 
-const App = () => (
-	<div>
-		<h1>Hello React</h1>
-	</div>
-);
+const App = () => {
+	const [criterion, setCriterion] = useState('');
+
+	return (
+		<Container>
+			<SearchBar criterion={criterion} setCriterion={setCriterion} />
+			<SongTable searchCriterion={criterion} />
+		</Container>
+	);
+};
 
 export default App;
