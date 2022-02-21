c613b28ad408890c12cbf31789d9841ded943f39
dstoffels <dan.stoffels@gmail.com>
2022-02-21 10:56:27 -0600

    implemented play/play count btn for SongRow component

--- src/Components/SongTable/SongRow/SongRow.jsx
+++ src/Components/SongTable/SongRow/SongRow.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Badge from '@mui/material/Badge';
@@ -8,9 +10,16 @@ import './SongRow.css';
 
 const SongRow = ({ song, editSong, openDeleteModal }) => {
 	const { title, album, artist, genre, release_date, likes } = song;
+	const dispatch = useDispatch();
 
 	const handleClick = e => {
 		editSong(song);
+	};
+
+	const handlePlay = async e => {
+		e.stopPropagation();
+		await axios.put(endpoint(`${song.id}/like`));
+		dispatch(getAllSongs());
 	};
 
 	const handleDeleteModal = e => {
@@ -21,15 +30,17 @@ const SongRow = ({ song, editSong, openDeleteModal }) => {
 	return (
 		<>
 			<tr onClick={handleClick} className='song-row'>
-				<td>{title}</td>
+				<td>
+					<Badge onClick={handlePlay} className='play-btn' color='secondary' badgeContent={likes}>
+						<PlayCircleFilledOutlinedIcon fontSize='large' color='success' />
+					</Badge>
+					{title}
+				</td>
 				<td>{artist}</td>
 				<td>{album}</td>
 				<td>{genre}</td>
 				<td>{release_date}</td>
 				<td>
-					<Badge color='secondary' badgeContent={likes}>
-						<PlayCircleFilledOutlinedIcon color='success' />
-					</Badge>
 					<Button color='error' onClick={handleDeleteModal}>
 						<DeleteIcon />
 					</Button>
@@ -41,8 +52,10 @@ const SongRow = ({ song, editSong, openDeleteModal }) => {
 
 // REDUX
 
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { editSong } from '../../EditModal/redux.js';
 import { openDeleteModal } from '../../DeleteModal/redux.js';
+import { endpoint } from '../../../API.js';
+import { getAllSongs } from '../redux.js';
 
 export default connect(null, { editSong, openDeleteModal })(SongRow);
